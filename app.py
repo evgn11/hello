cfeeda443a38d0a7ce198f94cf05aaf9c639756e
EV po <evgn11@mail.ru>
2021-10-01 13:35:34 +0000

    uwsqi version

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ import datetime
 import os
 
 def do_magic():
- now = datetime.datetime.now()
- return "Hello! {0}".format(now)
+  now = datetime.datetime.now()
+  return "Hello! {0}".format(now)
+
+def application(env, start_response):
+  start_response('200 OK', [('Content-Type', 'text/html')])
+  return [do_magic()]
+
 
 if __name__ == "__main__":
   if 'REQUEST_URI' in os.environ:
